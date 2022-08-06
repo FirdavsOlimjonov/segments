bf8163cef7a18ed1c0a9a8ef56a0457182790eac
Firdavs Olimjonov <firdavsolimjonov25@gmail.com>
2022-08-06 00:02:05 +0000

    Update Segment.java

--- src/main/java/com/epam/rd/autotasks/segments/Segment.java
+++ src/main/java/com/epam/rd/autotasks/segments/Segment.java
@@ -5,13 +5,17 @@ import static java.lang.Math.sqrt;
 import static java.lang.StrictMath.pow;
 
 class Segment {
+    Point start ;
+    Point end;
 
     public Segment(Point start, Point end) {
-
+        this.start = start;
+        this.end = end;
     }
 
     double length() {
-
+        double d;
+        
     }
 
     Point middle() {
