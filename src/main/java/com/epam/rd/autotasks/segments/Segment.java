609e01c81aa712342034365dfed5f56f6b7cd878
Firdavs Olimjonov <firdavsolimjonov25@gmail.com>
2022-08-06 00:33:46 +0000

    Update Segment.java

--- src/main/java/com/epam/rd/autotasks/segments/Segment.java
+++ src/main/java/com/epam/rd/autotasks/segments/Segment.java
@@ -14,17 +14,50 @@ class Segment {
     }
 
     double length() {
-        double d;
-        int x1 = start.x
+        int x1 = start.getX();
+        int y1 = start.getY();
+        int x2 = end.getX();
+        int y2 = end.getY();
+
+        return Math.sqrt(StrictMath.pow((x2-x1), 2)+StrictMath.pow((y2-y1), 2));
         
     }
 
     Point middle() {
+        int x1 = start.getX();
+        int y1 = start.getY();
+        int x2 = end.getX();
+        int y2 = end.getY();
+
+        int x = (x1+x2)/2;
+        int y = (y1+y2)/2;
+        return new Point(x,y);
 
     }
 
     Point intersection(Segment another) {
+        int x1 = start.getX();
+        int y1 = start.getY();
+        int x2 = end.getX();
+        int y2 = end.getY();
 
+        Point p1 = another.start;
+        Point p2 = another.end;
+
+        int x3 = p1.getX();
+        int y3= p1.getY();
+        int x4 = p2.getX();
+        int y4 = p2.getY();
+
+        Point po = null;
+        try{
+
+            int x = ((x1*y2 - y1*x2)(x3-x4)-(x1-x2)(x3*y4 - y3*x4))/((x1-x2)(y3-y4)-(y1-y2)(x3-x4));
+            int y = ((x1*y2 - y1*x2)(y3-y4)-(y1-y2)(x3*y4 - y3*x4))/((x1-x2)(y3-y4)-(y1-y2)(x3-x4));
+            po = new Point(x,y);
+        }finally{
+            return po;
+        }
     }
 
 }
